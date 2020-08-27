520b677d600968ce2e54aa5eb3d61d1a89bfde47
JuliusCaesar02 <69314234+JuliusCaesar02@users.noreply.github.com>
2020-08-27 19:08:07 +0200

    Add files via upload

---
+++ Gui.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+namespace Analisi_IP_SM_C
+{
+	public partial class Gui : Form
+	{
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Gui());
+		}
+		public Gui()
+		{
+			InitializeComponent();
+			button1.Click += new EventHandler(this.buttonClicked);
+		}
+
+		void buttonClicked(object sender, EventArgs e)
+		{
+			Sub sub = new Sub();
+			String auxString = null;
+			String ip = textBox1.Text;
+			sub.setIp(ip);
+			if (sub.decon(sub.setIp(ip))[0] == -1)
+			{
+				textBox2.Text = ("È stata riscontrata un'eccezione! Verifica il formato dell'IP" + "\r\n");
+				return;
+			}
+			else if (sub.decon(sub.setIp(ip))[0] == -2)
+			{
+				textBox2.Text = ("Errore! L'IP inserito non esiste" + "\n");
+				return;
+			}
+			sub.setClasse();
+			String sm = textBox3.Text;
+			sub.setSm(sm);
+			if (sub.decon(sub.setIp(sm))[0] == -1)
+			{
+				textBox2.Text = ("È stata riscontrata un'eccezione! Verifica il formato della SM" + "\r\n");
+				return;
+			}
+			else if (sub.decon(sub.setIp(sm))[0] == -2)
+			{
+				textBox2.Text = ("Errore! La SM inserita non esiste" + "\r\n");
+				return;
+			}
+			sub.setBinaryBit(sub.decon(sub.setIp(ip)));
+			textBox2.Text = ("Indirizzo IP binario: " + sub.getBinaryBit(sub.decon((sub.setIp(ip)))) + "\r\n");
+
+			sub.setBinaryBit(sub.decon((sub.setIp(sm))));
+			textBox2.AppendText("Indirizzo SM binario: " + sub.getBinaryBit(sub.decon((sub.setIp(sm)))) + "\r\n");
+			sub.decon(sub.setIp(ip));
+			textBox2.AppendText("1- La classe è " + sub.setClasse() + "\r\n");
+			if (sub.setClasse() == 'D' || sub.setClasse() == 'E')
+			{
+				textBox2.AppendText("Non è possibile eseguire ulteriori operazioni" + "\r\n");
+				return;
+			}
+			if (sub.setClasse() == 'X')
+			{
+				textBox2.AppendText("L'indirizzo IP inserito non esiste" + "\r\n");
+				return;
+			}
+			if (sub.setByteCrit() != 0)
+			{
+				textBox2.AppendText("2- Il byte critico è il " + (sub.setByteCrit()) + " (" + sub.decon(sm)[sub.setByteCrit() - 1] + ")" + "\r\n");
+			}
+			else textBox2.AppendText("2- Il byte critico non è presente" + "\r\n");
+
+			textBox2.AppendText("3- Il CIDR è /" + sub.setCidr() + "\r\n");
+			             
+			textBox2.AppendText("4- I bit di rete sono " + sub.setNetBit() + "\r\n");
+
+			sub.setSubBit();
+			if (sub.setSubBit() < 0)
+			{
+				textBox2.AppendText("5- ERRORE! I bit di sotto rete sono negativi! " + sub.setSubBit() + "\r\n");
+				return;
+			}
+			else textBox2.AppendText("5- I bit di sotto rete sono " + sub.setSubBit() + "\r\n");
+
+			sub.setHostBit();
+			if (sub.setHostBit() < 0)
+			{
+				textBox2.AppendText("6- ERRORE! I bit degli host sono negativi! " + sub.setHostBit() + "\r\n");
+				return;                                                                   
+			}
+			else textBox2.AppendText("6- I bit degli host sono " + sub.setHostBit() + "\r\n");
+
+			textBox2.AppendText("7- Ci possono essere " + sub.setHostESubPerNet(sub.setHostBit(), 2) + " host in ogni subnet (" + (sub.setHostESubPerNet(sub.setHostBit(), 0)) + ")" + "\r\n");
+
+			textBox2.AppendText("8- Ci possono essere " + sub.setHostESubPerNet(sub.setSubBit(), 0) + " subnet nella rete" + "\r\n");
+
+			textBox2.AppendText("9- Il magic number è " + sub.setMn() + "\r\n");
+
+			sub.setNetIp();
+			auxString = string.Join(".", sub.setNetIp());
+			textBox2.AppendText("10- Indirizzo di rete " +auxString + "\r\n");
+
+			sub.setBroadIp();
+			auxString = string.Join(".", sub.setBroadIp());
+			textBox2.AppendText("11- Indirizzo di broadcast " + auxString + "\r\n");
+
+			textBox2.AppendText("12- Primo indirizzo host " + sub.setRange(sub.setNetIp(), 1) + "\r\n");
+			textBox2.AppendText("      Ultimo indirizzo host " + sub.setRange(sub.setBroadIp(), -1) + "\r\n");
+
+			textBox2.AppendText("13- L'IP fa parte della subnet " + sub.setSubNet() + "\r\n");
+
+			textBox2.AppendText(sub.setBitMap());
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Text;
+
+public class Sub
+{
+	int i = 0;
+	class Analisi
+	{
+		public String ip;
+		public String sm;
+		public char classe;
+		public int netBit;
+		public int subBit;
+		public int hostBit;
+        public int[] netIpByte;
+        public int[] broadIpByte;
+	};
+	Analisi indirizzo = new Analisi();
+
+	public int[] decon(String IpSm)
+	{
+        String[] parts = IpSm.Split('.');
+		int[] aux;
+		aux = new int[4];
+		for (int i = 0; i < 4; i++)
+		{
+			try
+			{
+				aux[i] = Int16.Parse(parts[i]);
+				if (aux[i] > 255)
+				{
+					aux[0] = -2;
+					return aux;
+				}
+			}
+            catch (IndexOutOfRangeException) {
+			aux[0] = -1;
+			return aux;
+		}
+	}
+		return aux;
+	}
+	public String setSm(String sm)
+	{
+		indirizzo.sm = sm;
+		return indirizzo.sm;
+	}
+	public String setIp(String ip)
+	{
+		indirizzo.ip = ip;
+		return indirizzo.ip;
+	}
+	public char setClasse()
+	{
+		if (decon(indirizzo.ip)[0] > 0 && decon(indirizzo.ip)[0] <= 126)
+		{
+			indirizzo.classe = 'A';
+		}
+		else if (decon(indirizzo.ip)[0] >= 128 && decon(indirizzo.ip)[0] <= 191)
+		{
+			indirizzo.classe = 'B';
+		}
+		else if (decon(indirizzo.ip)[0] >= 192 && decon(indirizzo.ip)[0] <= 223)
+		{
+			indirizzo.classe = 'C';
+		}
+		else if (decon(indirizzo.ip)[0] >= 224 && decon(indirizzo.ip)[0] <= 239)
+		{
+			indirizzo.classe = 'D';
+		}
+		else if (decon(indirizzo.ip)[0] >= 240 && decon(indirizzo.ip)[0] <= 255)
+		{
+			indirizzo.classe = 'E';
+		}
+		else
+		{
+			indirizzo.classe = 'X';
+			return 'X';
+		}
+		return indirizzo.classe;
+	}
+	public int setByteCrit()
+	{
+		int aux = 0;
+		for (int i = 3; i >= 0; i--)
+		{
+			if (decon(indirizzo.sm)[i] == 128 || decon(indirizzo.sm)[i] == 192 || decon(indirizzo.sm)[i] == 224 || decon(indirizzo.sm)[i] == 240 || decon(indirizzo.sm)[i] == 248 || decon(indirizzo.sm)[i] == 252 || decon(indirizzo.sm)[i] == 254)
+			{
+				aux = i + 1;
+			}	
+		}
+		return aux;
+	}
+	public int setCidr()
+	{
+			int aux = 0;
+			int aux2 = 0;
+		for (i = 0; i < 4; i++)
+		{
+				aux = decon(indirizzo.sm)[i];
+				while (aux != 0)
+				{
+					aux2++;
+					aux &= aux - 1;
+				}
+			}
+		return aux2;
+	}
+	public int setNetBit()
+	{
+		switch (indirizzo.classe)
+		{
+			case 'A':
+				indirizzo.netBit = 8;
+				return 8;
+			case 'B':
+				indirizzo.netBit = 16;
+				return 16;
+			case 'C':
+				indirizzo.netBit = 24;
+				return 24;
+			default: return 1;
+		}
+	}
+	public int setSubBit()
+	{
+		indirizzo.subBit = setCidr() - indirizzo.netBit;
+		return indirizzo.subBit;
+	}
+	public int setHostBit()
+	{
+		indirizzo.hostBit = 32 - indirizzo.netBit - indirizzo.subBit;
+		return indirizzo.hostBit;
+	}
+	public long setHostESubPerNet(int aux, int aux2)
+	{
+		return (long)(Math.Pow(2, aux)) - aux2;
+	}
+	public int setMn()
+	{
+		return 256 - decon(indirizzo.sm)[setByteCrit() - 1];
+	}
+	public int[] setNetIp()
+	{
+		indirizzo.netIpByte = new int[4];
+		for (i = 0; i < 4; i++)
+		{
+			indirizzo.netIpByte[i] = decon(indirizzo.ip)[i] & decon(indirizzo.sm)[i];
+		}
+		return indirizzo.netIpByte;
+	}
+	public int[] setBroadIp()
+	{
+		int[] smComplement;
+		indirizzo.broadIpByte = new int[4];
+		smComplement = new int[4];
+		for (i = 0; i < 4; i++)
+		{
+			smComplement[i] = ~decon(indirizzo.sm)[i] & 0xff;
+			indirizzo.broadIpByte[i] = decon(indirizzo.ip)[i] | smComplement[i];
+		}
+		return indirizzo.broadIpByte;
+	}
+	public String setRange(int[] NetSubbyte, int importato)
+	{
+		int[] rangeAux;
+		rangeAux = new int[4];
+		for (i = 0; i < 3; i++)
+		{
+			rangeAux[i] = NetSubbyte[i];
+		}
+		rangeAux[3] = NetSubbyte[3] + importato;
+			return string.Join(".", rangeAux);
+		}
+	public String setBitMap()
+	{
+		String aux = null;
+		int j = 0;
+		for (i = 1; i <= indirizzo.netBit; i++)
+		{
+			aux += "n";
+			if (i % 8 == 0)
+			{
+				aux += ".";
+			}
+		}
+		j = i;
+		for (i = j; i <= indirizzo.subBit + indirizzo.netBit; i++)
+		{
+			aux += "s";
+			if (i % 8 == 0)
+			{
+				aux += ".";
+			}
+		}
+		j = i;
+		for (i = j; i <= indirizzo.hostBit + indirizzo.subBit + indirizzo.netBit; i++)
+		{
+			aux += "h";
+			if (i % 8 == 0 && i != 32)
+			{
+				aux += ".";
+			}
+		}
+		aux += "";
+		return aux;
+	}
+	public String setBinaryBit(int[] byteIPSM)
+	{
+		String aux = null;
+		String aux2 = null;
+		for (int j = 0; j <= 3; j++)
+		{
+				aux = Convert.ToString(byteIPSM[j], 2);
+				if (aux.Length < 8 * (1 + j))
+				{
+				for (i = aux.Length; i < 8; i++)
+				{
+					aux = "0" + aux;
+				}
+			}
+			aux2 += aux;
+		}
+		return aux2;
+	}
+	public String getBinaryBit(int[] byteIPSM)
+	{
+		StringBuilder aux = new StringBuilder();
+		aux.Append(setBinaryBit(byteIPSM));
+		for (i = 8; i < 32; i += 9)
+			aux.Insert(i, ".");
+		return aux.ToString();
+	}
+	public int setSubNet()
+	{
+		String aux = null;
+		aux = setBinaryBit(decon((indirizzo.ip))).Substring(indirizzo.netBit, indirizzo.subBit);
+		return Convert.ToInt32(aux, 2);
+	}
+}                    
